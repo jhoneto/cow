42797033b8b0dd70313e13751717ba797207f063
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2012-03-13 09:39:55 -0300

    iniciando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cow::Application.config.secret_token = 'cf08cea5b9fce5abaddfc4656068c7ed83db5c83fd3d91653bfe265b73b01846dc528697019812ffeae0d6d5fc93a4bddd900e15f8d7ee197f66277ef078dbcb'
