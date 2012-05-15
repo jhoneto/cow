9c7dcb9d1de02c7f86a82a796fc610f1ff654a48
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2012-05-15 18:34:03 -0300

    ajustando orçamento

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,13 @@ class HomeController < ApplicationController
 
 			r.add_field(:user_name, 'Hamilton Neto')
 
+			@list_of_itens = Group.all
+
+			r.add_table("TABLE_1", @list_of_itens, :header=>true) do |t|
+				t.add_column(:id)
+				t.add_column(:name)
+			end
+
 		end
 
 		report_file_name = report.generate
